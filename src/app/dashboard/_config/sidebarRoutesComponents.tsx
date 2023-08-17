cdccda3be857a4bfcaa47f335cb64742d7cc36bd
gustaveWPM <jgeorge@perso.be>
2023-08-17 22:00:04 +0200

    review: factored

--- src/app/dashboard/_config/sidebarRoutesComponents.tsx
+++ src/app/dashboard/_config/sidebarRoutesComponents.tsx
@@ -1,24 +1,20 @@
+import { IconBaseProps } from 'react-icons';
 import { RxActivityLog, RxDashboard, RxSketchLogo } from 'react-icons/rx';
 import { DashboardRoutesSidebarReactElements } from './utils/RoutesMapping';
 
 const p = { className: 'bg-purple-800 text-white my-4 p-3 rounded-lg inline-block' };
 const p2 = { size: 20 };
+
+const createSidebarComponent = (__SidebarIcon: React.ComponentType<IconBaseProps>) => (
+  <div {...p}>
+    <__SidebarIcon {...p2} />
+  </div>
+);
+
 export const dashboardRoutesSidebarComponents: DashboardRoutesSidebarReactElements = {
-  BASE_PAGE: (
-    <div {...p}>
-      <RxSketchLogo {...p2} />
-    </div>
-  ),
-  FOO_PAGE: (
-    <div {...p}>
-      <RxDashboard {...p2} />
-    </div>
-  ),
-  BAR_PAGE: (
-    <div {...p}>
-      <RxActivityLog {...p2} />
-    </div>
-  )
+  BASE_PAGE: createSidebarComponent(RxSketchLogo),
+  FOO_PAGE: createSidebarComponent(RxDashboard),
+  BAR_PAGE: createSidebarComponent(RxActivityLog)
 };
 
 export default dashboardRoutesSidebarComponents;
