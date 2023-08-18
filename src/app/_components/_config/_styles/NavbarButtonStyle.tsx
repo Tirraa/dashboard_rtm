0a07b1df5012791350b0925c7bc0586fd8fb8ccf
gustaveWPM <jgeorge@perso.be>
2023-08-18 20:04:23 +0200

    add: navbar and sidebar buttons transitions

--- src/app/_components/_config/_styles/NavbarButtonStyle.tsx
+++ src/app/_components/_config/_styles/NavbarButtonStyle.tsx
@@ -1,6 +1,6 @@
 import IButtonStyleBase from './_types/IButtonStyleBase';
 
-const commons = 'flex flex-1 justify-center rounded-md px-3 py-2 font-medium';
+const commons = 'transition-all flex flex-1 justify-center rounded-md px-3 py-2 font-medium';
 export const NavbarButtonStyle: IButtonStyleBase = {
   isActiveClassList: commons + ' ' + 'bg-gray-900 text-white',
   isNotActiveClassList: commons + ' ' + 'text-gray-300 hover:bg-gray-700 hover:text-white'
--- src/app/_components/_config/_styles/SidebarButtonStyle.tsx
+++ src/app/_components/_config/_styles/SidebarButtonStyle.tsx
@@ -1,6 +1,6 @@
 import IButtonStyleReactIcon from './_types/IButtonStyleReactIcon';
 
-const commons = 'text-white my-4 p-3 rounded-lg inline-block';
+const commons = 'transition-all text-white my-4 p-3 rounded-lg inline-block';
 export const SidebarButtonStyle: IButtonStyleReactIcon = {
   isActiveClassList: commons + ' ' + 'bg-purple-800',
   isNotActiveClassList: commons + ' ' + 'bg-purple-400',
