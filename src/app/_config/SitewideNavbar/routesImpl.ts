88a33bf844a01f17cd72726432cde892a52abf8d
gustaveWPM <jgeorge@perso.be>
2023-08-20 18:53:28 +0200

    review: minor changes

--- src/app/_config/SitewideNavbar/routesImpl.ts
+++ src/app/_config/SitewideNavbar/routesImpl.ts
@@ -1,8 +1,8 @@
-import getSlashEnvelope from '@/app/_lib/getSlashEnvelope';
 import { RoutesBase } from '../routes';
 import { SitewideNavbarRoutes, SitewideNavbarRoutesTitles } from './utils/RoutesMapping';
 
-const b = getSlashEnvelope(RoutesBase.sitewide);
+const b = RoutesBase.sitewide;
+// const b = getSlashEnvelope(RoutesBase.sitewide); // REM: Useless for now, since RoutesBase.sitewide is '/'
 
 // {ToDo} i18n this?
 export const sitewideNavbarRoutes: SitewideNavbarRoutes = {
