2b0c4a8325ae102a35c3ca0ad8782c6491037c4b
gustaveWPM <jgeorge@perso.be>
2023-08-20 15:19:02 +0200

    review: minor changes (consistency)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomepageInner from './_components/PagesInner/Homepage';
 
 export default function Home() {
   return (
-    <main className="fixed w-full h-screen flex flex-col text-center align-center justify-center">
+    <main className="fixed w-screen h-screen flex flex-col text-center align-center justify-center">
       <HomepageInner />
     </main>
   );
