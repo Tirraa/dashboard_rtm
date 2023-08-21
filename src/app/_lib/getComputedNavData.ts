98729e5b6a1943c3a34b8c0fa4ff08c134247c81
gustaveWPM <jgeorge@perso.be>
2023-08-21 04:37:12 +0200

    review: typo

--- src/app/_lib/getComputedNavData.ts
+++ src/app/_lib/getComputedNavData.ts
@@ -5,14 +5,14 @@ import RoutesTypesUnion from '../_types/RoutesTypesUnion';
 export function getComputedNavData(
   routes: RoutesTypesUnion,
   routesTitles: NavDataRoutesTitles,
-  dropDownConfig?: DropdownsConfig<keyof RoutesTypesUnion>
+  dropdownConfig?: DropdownsConfig<keyof RoutesTypesUnion>
 ): NavDataEntities {
   const computedNavData: NavDataEntities = Object.keys(routes).map((k) => {
     const k2 = k as keyof RoutesTypesUnion;
     const currentEntity: NavDataEntity = {
       getPath: routes[k2],
       getTitle: () => routesTitles[k2](),
-      ...(dropDownConfig && dropDownConfig[k2] ? { embeddedEntities: [...dropDownConfig[k2]] } : {})
+      ...(dropdownConfig && dropdownConfig[k2] ? { embeddedEntities: [...dropdownConfig[k2]] } : {})
     };
     return currentEntity;
   });
