e85b2bb1ef6b226e4a7c980b6d61a41b6c72158c
unknown <metodik383@gmail.com>
2023-08-21 23:16:26 +0200

    fix: added discord invite url

--- src/app/_components/PagesInner/Homepage.tsx
+++ src/app/_components/PagesInner/Homepage.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Discord as DiscordConfig } from '@/app/_config/discord';
 import { CogIcon } from '@heroicons/react/20/solid';
 import { Button } from '@material-tailwind/react';
 import Image from 'next/image';
@@ -14,12 +15,11 @@ const style: CSSProperties = {
 const [width, height]: [number, number] = [226, 69];
 
 // {ToDo} i18n this!
-// {ToDo} Implement bot invitation feature
 const HomepageInner: FunctionComponent<HomepageInnerProps> = () => (
   <div className="m-auto">
     <Image src="/rtm-txt-logo.svg" {...{ style, width, height }} alt="Rust Team Management (text logo)" />
     <h1 className="mt-2">Rust Team Management</h1>
-    <Link href="#invite-bot">
+    <Link href={DiscordConfig.botInviteLink} target="_blank">
       <Button className="mt-4 text-xl normal-case flex items-center gap-2" variant="gradient" size="lg">
         <CogIcon className="h-7 w-7" /> Inviter le bot
       </Button>
---
+++ src/app/_config/discord.ts
@@ -0,0 +1,5 @@
+export namespace Discord {
+  export const botInviteLink = 'https://discord.com/api/oauth2/authorize?client_id=1120800430897778700&permissions=8&scope=bot';
+}
+
+export default Discord;
