7d5933a09d96193cab6a0004a257fab88ce39f30
GustaveWPM <jgeorge@perso.be>
2023-09-22 04:25:23 +0200

    fix: dashboard pages static generation is back

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -1,9 +1,19 @@
 import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
+import { getStaticParams } from '@/i18n/server';
+import i18nTaxonomy from '@/taxonomies/i18n';
 import { LayoutBaseProps } from '@/types/Next';
+import { setStaticParamsLocale } from 'next-international/server';
 
 interface DashboardLayoutProps extends LayoutBaseProps {}
 
+export function generateStaticParams() {
+  return getStaticParams();
+}
+
 export default function DashboardLayout({ params, children }: DashboardLayoutProps) {
+  const lng = params[i18nTaxonomy.LANG_FLAG];
+  setStaticParamsLocale(lng);
+
   return (
     <div className="flex">
       <DashboardSidebar {...{ i18nProps: { ...params } }} />
