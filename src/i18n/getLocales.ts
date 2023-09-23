f7a214a49a54724a8b118d70645fddfba2999075
GustaveWPM <jgeorge@perso.be>
2023-09-23 07:20:42 +0200

    review: LocalesGetterConfigObjDiscreteType -> LocalesGetterConfigObjTypeConstraint

--- src/i18n/getLocales.ts
+++ src/i18n/getLocales.ts
@@ -1,9 +1,9 @@
-import { LocalesGetterConfigObjDiscreteType } from '@/types/i18n';
+import { LocalesGetterConfigObjTypeConstraint } from '@/types/i18n';
 
 export const getLocales = () =>
   ({
     fr: () => import('@/i18n/locales/fr'),
     en: () => import('@/i18n/locales/en')
-  } satisfies LocalesGetterConfigObjDiscreteType);
+  } satisfies LocalesGetterConfigObjTypeConstraint);
 
 export default getLocales;
