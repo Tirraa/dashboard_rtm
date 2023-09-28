4fe425985f49efa82dff0095be0d172258f7878e
GustaveWPM <jgeorge@perso.be>
2023-09-28 13:02:55 +0200

    fix: wrongly wrote RegEx, OoOops! (I hate this)

--- packages/static-analyzer/src/validators/categoriesConvention.ts
+++ packages/static-analyzer/src/validators/categoriesConvention.ts
@@ -1,9 +1,9 @@
 export function isValidCategoryOrSubcategory(s: string): boolean {
-  const regex = /^(?=.*[a-z0-9])[-_a-z0-9]+$/;
+  const regex = /^[a-z0-9][-a-z0-9]*$/;
   return regex.test(s.toLowerCase());
 }
 
 export const NAMING_CONSTRAINTS_MSG =
-  'Only dashes, underscores and alphanumerical characters are allowed! You also MUST use at least one letter or digit.';
+  'Only dashes and alphanumeric characters are allowed, with the requirement that the first character MUST be a letter or a digit.';
 
 export default isValidCategoryOrSubcategory;
