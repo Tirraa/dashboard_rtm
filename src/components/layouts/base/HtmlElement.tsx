11b4ef0ddafa108dac889203d97fb031f5ac5d33
GustaveWPM <jgeorge@perso.be>
2023-10-02 21:17:39 +0200

    fix: refactored theme initialization

--- src/components/layouts/base/HtmlElement.tsx
+++ src/components/layouts/base/HtmlElement.tsx
@@ -4,18 +4,17 @@ import SplashScreen from '@/components/shared/misc/SplashScreen';
 import SitewideNavbar from '@/components/shared/navbar/SitewideNavbar';
 import PROGRESSBAR_CONFIG from '@/config/progressbar';
 import I18nProvider from '@/contexts/I18nProvider';
-import { isServerCtx } from '@/lib/next';
 import i18nTaxonomy from '@/taxonomies/i18n';
 import initializeTheme from '@/themes/retrieveOrInferTheme';
 import { LayoutBaseProps } from '@/types/Next';
 import { SessionProvider } from 'next-auth/react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import NextTopLoader from './NextTopLoader';
 
 interface HtmlElementProps extends LayoutBaseProps {}
 
 const HtmlElement: FunctionComponent<HtmlElementProps> = ({ children, params }) => {
-  if (!isServerCtx()) initializeTheme();
+  useEffect(() => initializeTheme(), []);
 
   const locale = params[i18nTaxonomy.LANG_FLAG];
   return (
--- src/lib/next.ts
+++ src/lib/next.ts
@@ -3,5 +3,3 @@ import { NextFont } from 'next/dist/compiled/@next/font';
 
 export const fCls = (f: NextFont): ClassName => ({ className: f.className });
 export const fClStr = (f: NextFont): string => f.className;
-
-export const isServerCtx = (): boolean => typeof window === 'undefined' || typeof document === 'undefined';
