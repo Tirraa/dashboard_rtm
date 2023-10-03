d3c530798feb59afe6d29fb2974bffeaae364057
GustaveWPM <jgeorge@perso.be>
2023-10-04 00:14:55 +0200

    fix: dashboard sidebar collapse button color on light/dark mode

--- src/components/config/styles/SitebarDropdownButtonStyle.ts
+++ src/components/config/styles/SitebarDropdownButtonStyle.ts
@@ -1,6 +1,6 @@
 import IButtonStyleBase from './types/IButtonStyleBase';
 
-const COMMONS = 'bg-black rounded-full w-8 h-8 p-1 lg:mt-0';
+const COMMONS = 'bg-black rounded-full w-8 h-8 p-1 lg:mt-0 text-white';
 
 export const SitebarDropdownButtonStyle: IButtonStyleBase = {
   isActiveClassList: COMMONS + ' ' + '-mt-2 lg:-mr-1',
