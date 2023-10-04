bb155a20d5a417167ece47ea02b4f2841ded1005
GustaveWPM <jgeorge@perso.be>
2023-10-04 04:04:29 +0200

    fix: appended exclude logic in next-sitemap-config custom transformation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,12 @@
 const siteUrl = process.env.NEXTAUTH_URL;
 const DEFAULT_LANGUAGE = 'fr';
+const EXCLUDED_ROUTES_PREFIXES = ['/favicon.ico', '/icon.svg', '/apple-icon.png', '/dashboard'];
+
+const getPathWithoutI18nPart = (path) => path.substring(path.indexOf('/', path.indexOf('/') + 1));
+
+const isIgnoredPath = (path) =>
+  EXCLUDED_ROUTES_PREFIXES.some((prefix) => getPathWithoutI18nPart(path).startsWith(prefix)) ||
+  EXCLUDED_ROUTES_PREFIXES.some((prefix) => path.startsWith(prefix));
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
@@ -12,6 +19,8 @@ module.exports = {
     const defaultLanguageNeedle = '/' + DEFAULT_LANGUAGE;
     const defaultLanguageEnvelopeNeedle = defaultLanguageNeedle + '/';
     const defaultLanguageNeedleLen = defaultLanguageNeedle.length;
+
+    if (isIgnoredPath(path)) path = '';
 
     if (path.startsWith(defaultLanguageEnvelopeNeedle)) {
       path = path.substring(defaultLanguageNeedleLen);
