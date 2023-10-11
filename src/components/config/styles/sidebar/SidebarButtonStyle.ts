02d7ff9308dcf05b1c355ba851efb0ae1813e007
GustaveWPM <jgeorge@perso.be>
2023-10-11 06:47:30 +0200

    review: removed dummy testing colors on dashboard sidebar buttons

--- src/components/config/styles/sidebar/SidebarButtonStyle.ts
+++ src/components/config/styles/sidebar/SidebarButtonStyle.ts
@@ -1,10 +1,10 @@
 import IButtonStyleReactIcon from '@/components/config/styles/types/IButtonStyleReactIcon';
 
-const COMMONS = 'transition-colors duration-200 p-2.5 rounded-lg text-white';
+const COMMONS = 'transition-colors duration-200 p-2.5 rounded-lg';
 
 export const SidebarButtonStyle: IButtonStyleReactIcon = {
-  isActiveClassList: COMMONS + ' ' + 'bg-purple-800 dark:text-white dark:bg-slate-600',
-  isNotActiveClassList: COMMONS + ' ' + 'bg-purple-400 dark:text-gray-300 dark:bg-slate-800',
+  isActiveClassList: COMMONS + ' ' + 'text-white bg-slate-600',
+  isNotActiveClassList: COMMONS + ' ' + 'text-gray-300 bg-slate-800',
   sidebarIconProps: { size: 20 }
 } as const;
 
