513a9f7c94fe074f6467ac8e343861c1a79fb849
GustaveWPM <jgeorge@perso.be>
2023-11-03 21:28:20 +0100

    fix(dashboard): broken scroll

--- src/components/hooks/useLockScreenScrollY.ts
+++ src/components/hooks/useLockScreenScrollY.ts
@@ -4,9 +4,10 @@ const EFFECT_CLASSES = ['h-100svh', 'overflow-y-hidden'];
 
 const useLockScreenScrollY = () =>
   useLayoutEffect(() => {
-    document.documentElement.classList.add(...EFFECT_CLASSES);
+    const getTarget = () => document.body;
+    getTarget().classList.add(...EFFECT_CLASSES);
 
-    return () => document.documentElement.classList.remove(...EFFECT_CLASSES);
+    return () => getTarget().classList.remove(...EFFECT_CLASSES);
   }, []);
 
 export default useLockScreenScrollY;
