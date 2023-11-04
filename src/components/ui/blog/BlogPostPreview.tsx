87a233f69b4810f189bce9d5a5a5c9a0ae78eba1
GustaveWPM <jgeorge@perso.be>
2023-11-04 05:23:18 +0100

    style(BlogPostPreview): removed scale effect on mobiles design, added some more hover and focus sfx

--- src/components/ui/blog/BlogPostPreview.tsx
+++ src/components/ui/blog/BlogPostPreview.tsx
@@ -17,9 +17,9 @@ export const BlogPostPreview: FunctionComponent<BlogPostPreviewProps> = ({ post,
     <article>
       <Link
         href={getBlogPostPathWithoutI18nPart(post)}
-        className="flex h-full w-full flex-col transition-transform duration-300 hover:scale-105 hover:delay-0 hover:duration-100 focus:scale-105 focus:delay-0 focus:duration-100"
+        className="flex h-full w-full flex-col transition-transform duration-300 hover:delay-0 hover:duration-100 focus:delay-0 focus:duration-100 dark:hover:brightness-125 dark:focus:brightness-125 lg:hover:scale-105 lg:focus:scale-105 lg:focus:scale-105"
       >
-        <Card className="overflow-hidden rounded shadow-lg">
+        <Card className="overflow-hidden rounded shadow-lg transition-[box-shadow] duration-300 hover:shadow-xl focus:shadow-xl">
           <CardHeader className="pb-2">
             <CardTitle titleType={isNotOnBlogSubcategoryPage ? 'h3' : 'h2'} className="is-h3">
               {post.title}
