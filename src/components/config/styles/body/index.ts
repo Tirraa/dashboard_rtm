a1d66025c734b3a41eb3f01e2e19e82f62ac09e9
GustaveWPM <jgeorge@perso.be>
2023-11-04 05:00:50 +0100

    fix(Body Container): scrollbar gutter glitch

--- src/components/config/styles/body/index.ts
+++ src/components/config/styles/body/index.ts
@@ -1,3 +1,3 @@
 export const BODY_CLS = 'overflow-x-hidden bg-background font-sans antialiased';
-export const BODY_CONTAINER_CLS = 'min-w-screen flex min-h-screen flex-col';
+export const BODY_CONTAINER_CLS = 'min-h-screen min-w-screen w-screen max-w-screen flex flex-col';
 export default BODY_CLS;
