c3d985765d90a81e8236c259b78e66164bd4936b
GustaveWPM <jgeorge@perso.be>
2023-11-07 21:01:15 +0100

    style(buttons): contrast adjustments

--- src/components/config/styles/buttons/index.ts
+++ src/components/config/styles/buttons/index.ts
@@ -7,7 +7,7 @@ type TButtonConfig = {
 export const BUTTON_CONFIG: TButtonConfig = {
   CLASSNAME: 'font-bold',
   NOT_ACTIVE_CLASSNAME:
-    'text-muted-foreground bg-transparent focus:bg-accent hover:bg-accent focus:text-black hover:text-black dark:hover:text-primary-foreground dark:focus:text-primary-foreground',
+    'text-white text-opacity-75 dark:text-opacity-100 dark:text-muted-foreground bg-transparent focus:bg-accent hover:bg-accent focus:text-black hover:text-black dark:hover:text-primary-foreground dark:focus:text-primary-foreground',
   ACTIVE_CLASSNAME: 'bg-primary text-primary-foreground'
 } as const;
 
