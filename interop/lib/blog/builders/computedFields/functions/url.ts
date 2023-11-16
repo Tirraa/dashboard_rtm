a54971bd49df14b8928c540c39e1ec95b48e8553
GustaveWPM <jgeorge@perso.be>
2023-11-16 19:18:49 +0100

    fix: blog post URL builder

--- interop/lib/blog/builders/computedFields/functions/url.ts
+++ interop/lib/blog/builders/computedFields/functions/url.ts
@@ -1,5 +1,17 @@
 import type { PostBase } from '@/types/Blog';
 import type { AppPath } from '@/types/Next';
 
-export const buildBlogPostUrl = (post: PostBase): AppPath => `/${post._raw.flattenedPath}`;
+export const buildBlogPostUrl = (post: PostBase): AppPath => {
+  const OPTIONAL_LOCALE_PART_INDEX = 2;
+
+  const flattenedPath = post._raw.flattenedPath;
+  const flattenedPathParts = flattenedPath.split('/');
+  if (flattenedPathParts.length <= OPTIONAL_LOCALE_PART_INDEX + 1) return '/' + flattenedPath;
+
+  const localePart = flattenedPathParts[OPTIONAL_LOCALE_PART_INDEX];
+  flattenedPathParts.splice(OPTIONAL_LOCALE_PART_INDEX, 1);
+  flattenedPathParts.unshift(localePart);
+  return '/' + flattenedPathParts.join('/');
+};
+
 export default buildBlogPostUrl;
