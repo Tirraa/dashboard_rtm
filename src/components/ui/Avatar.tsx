3e273c9c910c1760e516503a2e05237b4170f2f8
GustaveWPM <jgeorge@perso.be>
2023-11-28 05:59:22 +0100

    chore: removed unused dep

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/tailwind';
 
 const Avatar = React.forwardRef<React.ElementRef<typeof AvatarPrimitive.Root>, React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>>(
   ({ className, ...props }, ref) => (
-    <AvatarPrimitive.Root ref={ref} className={cn('rounded-full relative flex h-10 w-10 shrink-0 overflow-hidden', className)} {...props} />
+    <AvatarPrimitive.Root ref={ref} className={cn('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)} {...props} />
   )
 );
 Avatar.displayName = AvatarPrimitive.Root.displayName;
@@ -21,7 +21,7 @@ const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
 >(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Fallback ref={ref} className={cn('rounded-full flex h-full w-full items-center justify-center bg-muted', className)} {...props} />
+  <AvatarPrimitive.Fallback ref={ref} className={cn('flex h-full w-full items-center justify-center rounded-full bg-muted', className)} {...props} />
 ));
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
