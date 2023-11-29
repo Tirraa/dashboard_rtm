308a72f5aa0885d543f15da304020f89be9bec00
GustaveWPM <jgeorge@perso.be>
2023-11-29 22:45:26 +0100

    chore: updated typescript

--- src/i18n/getLocales.ts
+++ src/i18n/getLocales.ts
@@ -3,7 +3,7 @@ import type { LocalesGetterConfigObjTypeConstraint, LocalesObj, LocalesObjEntity
 
 // * ... https://stackoverflow.com/questions/77315286/is-it-possible-to-keep-a-strict-type-argument-on-promise-when-using-an-import & https://github.com/microsoft/TypeScript/issues/56242
 export const GENERATED_LOCALES_OBJ = Object.fromEntries(
-  LANGUAGES.map((language) => [language, () => import(`@/i18n/locales/${language}`)] satisfies LocalesObjEntity)
+  LANGUAGES.map((language) => [language, () => import(`@/i18n/locales/${language}` as const)] satisfies LocalesObjEntity)
 ) as LocalesObj satisfies LocalesGetterConfigObjTypeConstraint;
 
 export default GENERATED_LOCALES_OBJ;
