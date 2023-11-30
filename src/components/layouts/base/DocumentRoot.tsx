da14512370ca7b2ea1a4c0f0f467d0599f1f3137
GustaveWPM <jgeorge@perso.be>
2023-11-30 06:13:55 +0100

    revert(ProgressBar): removed progress bar since it isn't compatible with the new version of next

--- src/components/layouts/base/DocumentRoot.tsx
+++ src/components/layouts/base/DocumentRoot.tsx
@@ -3,7 +3,6 @@ import BODY_CLS, { BODY_CONTAINER_CLS } from '@/components/config/styles/body';
 import { HTML_STYLE } from '@/components/config/styles/html';
 import SitewideNavbar from '@/components/ui/navbar/SitewideNavbar';
 import ELEMENTS_ID from '@/config/elementsId';
-import PROGRESSBAR_CONFIG from '@/config/progressbar';
 import Providers from '@/contexts/Providers';
 import { fInter } from '@/fonts';
 import { fcn } from '@/lib/next';
@@ -11,14 +10,12 @@ import { cn } from '@/lib/tailwind';
 import type { LayoutBaseProps } from '@/types/Next';
 import Locale from 'intl-locale-textinfo-polyfill';
 import type { FunctionComponent } from 'react';
-import NextTopLoader from './NextTopLoader';
 
 interface DocumentRootProps extends LayoutBaseProps {
   withNavbar?: boolean;
-  disableTopLoader?: boolean;
 }
 
-export const DocumentRoot: FunctionComponent<DocumentRootProps> = ({ children, params, withNavbar, disableTopLoader }) => {
+export const DocumentRoot: FunctionComponent<DocumentRootProps> = ({ children, params, withNavbar }) => {
   const language = params[I18nTaxonomy.LANGUAGE];
   const { direction: dir } = new Locale(language).textInfo;
 
@@ -27,7 +24,6 @@ export const DocumentRoot: FunctionComponent<DocumentRootProps> = ({ children, p
       <body className={cn(BODY_CLS, fcn(fInter))} style={{ scrollbarGutter: 'stable' }}>
         <div id={ELEMENTS_ID.BODY_CONTAINER} className={BODY_CONTAINER_CLS}>
           <Providers locale={language}>
-            {!disableTopLoader && <NextTopLoader {...PROGRESSBAR_CONFIG} />}
             {withNavbar && <SitewideNavbar />}
             {children}
           </Providers>
