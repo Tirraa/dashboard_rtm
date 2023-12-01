876221361eebb7aeab3183041076f6b255a663a6
GustaveWPM <jgeorge@perso.be>
2023-12-01 03:01:49 +0100

    chore: better i18ns checker errors msg accuracy

--- interop/config/i18n.ts
+++ interop/config/i18n.ts
@@ -23,14 +23,33 @@ export const i18ns: ValidateI18ns<typeof _i18ns> = _i18ns;
 export const LANGUAGES: LanguageFlag[] = getEnumKeys(ELanguagesFlag);
 export const DEFAULT_LANGUAGE: LanguageFlag = DEFAULT_LANGUAGE_OBJ._infos.lng;
 
-// * ... Magic
-
-type ValidateI18ns<T extends I18ns> = keyof VocabType extends keyof FlipI18ns<T>
-  ? T
-  : '✗ Missing value: i18ns must include all the keys of VocabType, exhaustively, as its values.';
-
-type FlipI18ns<T extends I18ns> = {
-  [P in keyof T as T[P]]: P;
-};
+// * ... Checkers
 
 type I18ns = Record<PropertyKey, keyof VocabType>;
+
+type ValidateI18ns<I18NS_CONST extends I18ns> = keyof VocabType extends keyof FlipI18ns<I18NS_CONST>
+  ? I18NS_CONST
+  : typeof _i18ns extends I18ns
+    ? `Missing value: ${GetMissingValues<VocabType, FlipI18ns<typeof _i18ns>>['PWNED_BY_I18NS_CONST_IS_NOT_EXHAUSTIVE']}`
+    : 'INVALID I18NS';
+
+// * ... Checkers magic helpers
+
+type FlipI18ns<I18NS_CONST extends I18ns> = {
+  [P in keyof I18NS_CONST as I18NS_CONST[P]]: P;
+};
+
+type DiffKeys<OBJ_A extends object, OBJ_B extends object> = {
+  [K in Exclude<keyof OBJ_A, keyof OBJ_B>]: K;
+};
+
+type ComputeMissingValues<VOCAB_TYPE extends VocabType, FLIPPED_I18NS_CONST extends object> = {
+  PWNED_BY_I18NS_CONST_IS_NOT_EXHAUSTIVE: keyof DiffKeys<VOCAB_TYPE, FLIPPED_I18NS_CONST>;
+};
+
+type GetMissingValues<VOCAB_TYPE extends VocabType, FLIPPED_I18NS_CONST extends object> = keyof DiffKeys<
+  VOCAB_TYPE,
+  FLIPPED_I18NS_CONST
+> extends never
+  ? never
+  : ComputeMissingValues<VOCAB_TYPE, FLIPPED_I18NS_CONST>;
