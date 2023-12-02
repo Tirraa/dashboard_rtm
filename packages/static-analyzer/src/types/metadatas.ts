5224413d029db7262a93c1ce7878a63061a4ea6b
GustaveWPM <jgeorge@perso.be>
2023-12-02 17:31:39 +0100

    chore: editorconfig dotfile and minor changes

--- packages/static-analyzer/src/types/metadatas.ts
+++ packages/static-analyzer/src/types/metadatas.ts
@@ -1,4 +1,3 @@
-export type UnknownI18nJSONObj = Record<string, unknown>;
 export type Subcategory = string;
 export type Category = string;
 
@@ -7,6 +6,7 @@ export type CategoriesMetadatas = Record<Category, Subcategories>;
 export type DeclaredCategoriesMetadatas = CategoriesMetadatas;
 
 type UnknownI18nJSONProp = string | UnknownI18nJSONObj;
+export type UnknownI18nJSONObj = Record<string, unknown>;
 export type I18nJSONPart = Record<string, UnknownI18nJSONProp>;
 
 export type ErrorsDetectionFeedback = string;
--- src/lib/functions/typescript/getEnumKeys.ts
+++ src/lib/functions/typescript/getEnumKeys.ts
@@ -1,5 +1,4 @@
-export const getEnumKeys = <T extends Record<string, unknown>>(enumerableObject: T): (keyof T)[] => {
-  return Object.keys(enumerableObject).filter((key) => isNaN(Number(key))) as (keyof T)[];
-};
+export const getEnumKeys = <T extends Record<string, unknown>>(enumerableObject: T): (keyof T)[] =>
+  Object.keys(enumerableObject).filter((key) => isNaN(Number(key))) as (keyof T)[];
 
 export default getEnumKeys;
