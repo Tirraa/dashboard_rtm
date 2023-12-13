ab288e567d34eada967ca8d3950694b4d1a873b2
GustaveWPM <jgeorge@perso.be>
2023-12-13 12:58:36 +0100

    fix: ugly progressbar height

--- src/config/progressbar.ts
+++ src/config/progressbar.ts
@@ -3,7 +3,7 @@ import type { NextTopLoaderProps } from '@/components/layouts/base/NextTopLoader
 const PROGRESSBAR_CONFIG: NextTopLoaderProps = {
   color: 'rgba(255, 255, 255, .15)',
   showSpinner: false,
-  height: 5,
+  height: 3,
   crawlSpeed: 50,
   speed: 275,
   initialPosition: 0.5
