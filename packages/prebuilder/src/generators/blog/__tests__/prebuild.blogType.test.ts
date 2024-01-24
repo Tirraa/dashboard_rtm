926486761024a8230f97a5dca4580adcf4037920
GustaveWPM <jgeorge@perso.be>
2024-01-24 06:19:31 +0100

    chore: updated blogtype generation test with some 'index' files

--- packages/prebuilder/src/generators/blog/__tests__/prebuild.blogType.test.ts
+++ packages/prebuilder/src/generators/blog/__tests__/prebuild.blogType.test.ts
@@ -30,8 +30,8 @@ describe('generateBlogType (formatted)', () => {
             fr: ['fake-post-01']
           },
           'fake-subcategory-three': {
-            DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'],
-            es: ['fake-post-01'],
+            DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'],
+            es: ['fake-post-01', 'index'],
             zh: ['fake-post-02']
           },
           'fake-subcategory-two': { fr: ['fake-post-02'] }
@@ -73,8 +73,8 @@ describe('generateBlogType (ugly)', () => {
       {
         'fake-category-one': {
           'fake-subcategory-one': {
-            DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'],
-            en: ['fake-post-01', 'fake-post-03'],
+            DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'],
+            en: ['fake-post-01', 'fake-post-03', 'index'],
             it: ['fake-post-01', 'fake-post-02'],
             fr: ['fake-post-01']
           },
@@ -86,14 +86,20 @@ describe('generateBlogType (ugly)', () => {
           'fake-subcategory-two': { fr: ['fake-post-02'] }
         },
         'fake-category-three': {
-          'fake-subcategory-five': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'], en: ['fake-post-03'], it: ['fake-post-03'] },
-          'fake-subcategory-four': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'], es: ['fake-post-04'] }
+          'fake-subcategory-five': {
+            DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'],
+            en: ['fake-post-03'],
+            it: ['fake-post-03']
+          },
+          'fake-subcategory-four': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'], es: ['fake-post-04', 'index'] }
         },
         'fake-empty-subcategories-nested-in-category': {
-          'fake-subcategory-seven': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'], zh: ['fake-post-10'] },
+          'fake-subcategory-seven': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'], zh: ['fake-post-10', 'index'] },
           'fake-empty-subcategory': {}
         },
-        'fake-category-two': { 'fake-subcategory-six': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03'], zh: ['fake-post-10'] } },
+        'fake-category-two': {
+          'fake-subcategory-six': { DEFAULT_LANGUAGE: ['fake-post-01', 'fake-post-02', 'fake-post-03', 'index'], zh: ['fake-post-10'] }
+        },
         'fake-empty-category': {}
       },
       pretty,
