43a65704e8d357e3e5aba652b5cf1674dc00188a
GustaveWPM <jgeorge@perso.be>
2024-01-29 19:51:40 +0100

    chore: updated stryker

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -1,12 +1,10 @@
 /* v8 ignore start */
 // Stryker disable all
 // @ts-check
-const SOON_OBSOLETE_IGNORE_PATTERNS = ['.next']; // {ToDo} Remove me when this will be released: https://github.com/stryker-mutator/stryker-js/issues/4571
 
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
   plugins: ['@stryker-mutator/vitest-runner', '@stryker-mutator/typescript-checker'],
-  ignorePatterns: [...SOON_OBSOLETE_IGNORE_PATTERNS, 'prebuilder-dist'],
   typescriptChecker: {
     prioritizePerformanceOverAccuracy: false
   },
@@ -14,6 +12,7 @@ const config = {
     configFile: 'vitest.config.mts',
     dir: '.'
   },
+  ignorePatterns: ['prebuilder-dist'],
   tsconfigFile: 'tsconfig.json',
   checkers: ['typescript'],
   testRunner: 'vitest',
