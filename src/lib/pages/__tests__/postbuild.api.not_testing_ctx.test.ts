d82ca0dae0e7fef6a308be638ebd8d397a132422
GustaveWPM <jgeorge@perso.be>
2024-01-30 19:13:17 +0100

    chore: added pages disabled testing ctx tests

---
+++ src/lib/pages/__tests__/postbuild.api.not_testing_ctx.test.ts
@@ -0,0 +1,26 @@
+import { DEFAULT_LANGUAGE } from '##/config/i18n';
+import { describe, expect, vi, it } from 'vitest';
+import PagesConfig from '@/config/pages';
+
+import { getPageByLanguageAndPathStrict } from '../api';
+
+vi.mock('../ctx', async (orgImport) => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  const mod = await orgImport<typeof import('../ctx')>();
+
+  return {
+    default: {
+      ...mod.default,
+      TESTING: false
+    }
+  } satisfies typeof mod;
+});
+
+describe('getPageByLanguageAndPathStrict (unhappy paths)', () => {
+  it('should return null, given testing path in not testing ctx', () => {
+    const page = getPageByLanguageAndPathStrict(DEFAULT_LANGUAGE, `${PagesConfig.TESTING_ROOT}/fake-page-00`);
+    expect(page).toBe(null);
+  });
+});
+
+vi.doUnmock('../ctx');
