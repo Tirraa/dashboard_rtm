45b2999d907e09143cc4a24d43ef43caa4ac3798
GustaveWPM <jgeorge@perso.be>
2024-02-01 21:29:03 +0100

    chore: minor changes

--- src/types/Adapters/PageAdapter.ts
+++ src/types/Adapters/PageAdapter.ts
@@ -5,27 +5,33 @@ import type { PagesFromCodegenSchema } from '@rtm/generated/Pages';
 import type { LanguageFlag } from '@rtm/shared-types/I18n';
 import type { DefaultLanguage } from '##/config/i18n';
 
+type IndexToken = typeof INDEX_TOKEN;
+type PathSeparator = '/';
+type TopLevelRoot = '/';
+
 type PageAdapter<P extends PagesFromCodegenSchema> = P extends { head: LanguageFlag }
   ? P extends { head: DefaultLanguage }
     ? {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        url: P['url'] extends `/${DefaultLanguage}/${infer _}` ? P['url'] : `/${DefaultLanguage}${P['url']}`;
+        url: P['url'] extends `${TopLevelRoot}${DefaultLanguage}${PathSeparator}${infer _}`
+          ? P['url']
+          : `${TopLevelRoot}${DefaultLanguage}${P['url']}`;
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        path: P['path'] extends `${infer _}/${typeof INDEX_TOKEN}` ? typeof INDEX_TOKEN : P['path'];
-        root: P['head'] extends DefaultLanguage ? '/' : P['head'];
+        path: P['path'] extends `${infer _}${PathSeparator}${IndexToken}` ? IndexToken : P['path'];
+        root: P['head'] extends DefaultLanguage ? TopLevelRoot : P['head'];
         lang: DefaultLanguage;
       }
     : {
-        path: P['pathWithoutHead'] extends `${infer Head}/${typeof INDEX_TOKEN}` ? Head : P['pathWithoutHead'];
-        root: P['nestingLevelTwo'] extends '' ? '/' : P['nestingLevelTwo'];
+        path: P['pathWithoutHead'] extends `${infer Head}${PathSeparator}${IndexToken}` ? Head : P['pathWithoutHead'];
+        root: P['nestingLevelTwo'] extends '' ? TopLevelRoot : P['nestingLevelTwo'];
         lang: P['head'];
         url: P['url'];
       }
   : {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      url: P['url'] extends `/${DefaultLanguage}/${infer _}` ? P['url'] : `/${DefaultLanguage}${P['url']}`;
-      path: P['path'] extends `${infer Head}/${typeof INDEX_TOKEN}` ? Head : P['path'];
-      root: P['path'] extends `${P['head']}/${typeof INDEX_TOKEN}` ? '/' : P['head'];
+      url: P['url'] extends `${TopLevelRoot}${DefaultLanguage}${PathSeparator}${infer _}` ? P['url'] : `${TopLevelRoot}${DefaultLanguage}${P['url']}`;
+      root: P['path'] extends `${P['head']}${PathSeparator}${IndexToken}` ? TopLevelRoot : P['head'];
+      path: P['path'] extends `${infer Head}${PathSeparator}${IndexToken}` ? Head : P['path'];
       lang: DefaultLanguage;
     };
 
