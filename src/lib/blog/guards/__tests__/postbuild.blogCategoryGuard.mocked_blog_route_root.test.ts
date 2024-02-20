4280f77436db6cd43333122237e97e391d2ed04f
GustaveWPM <jgeorge@perso.be>
2024-02-20 23:08:40 +0100

    fix: oops

--- src/lib/blog/guards/__tests__/postbuild.blogCategoryGuard.mocked_blog_route_root.test.ts
+++ src/lib/blog/guards/__tests__/postbuild.blogCategoryGuard.mocked_blog_route_root.test.ts
@@ -26,8 +26,8 @@ vi.mock('##/config/routes', async (orgImport) => {
 
 describe('blogPostGuard', () => {
   it('should not throw, given valid input', async () => {
-    expect(
-      await blogPostGuard({
+    await expect(
+      blogPostGuard({
         params: {
           [BlogTaxonomy.SUBCATEGORY]: TESTING_BLOG_FAKE_SUBCATEGORY,
           [BlogTaxonomy.CATEGORY]: BlogConfig.TESTING_CATEGORY,
