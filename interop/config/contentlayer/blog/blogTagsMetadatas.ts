4e9f6a60e01f0032187f4ba21795da7c4f9eabbf
GustaveWPM <jgeorge@perso.be>
2024-03-05 09:42:23 +0100

    chore: refactor

---
+++ interop/config/contentlayer/blog/blogTagsMetadatas.ts
@@ -0,0 +1,5 @@
+import { generateBlogTagOptionsVocabSchema, generateIndexedBlogTagOptions } from '../../../lib/builders/unifiedImport';
+
+export const indexedBlogTagOptions = generateIndexedBlogTagOptions();
+
+export const blogTagOptionsVocabSchema = generateBlogTagOptionsVocabSchema();
