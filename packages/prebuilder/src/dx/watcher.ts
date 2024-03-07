41081890bcfda9538d0e4ad8713ab65adafaa6ab
GustaveWPM <jgeorge@perso.be>
2024-03-07 06:23:12 +0100

    chore: minor changes

--- packages/prebuilder/src/dx/watcher.ts
+++ packages/prebuilder/src/dx/watcher.ts
@@ -1,25 +1,25 @@
 /* v8 ignore start */
 // Stryker disable all
 
-import chokidar from 'chokidar';
+import { watch } from 'chokidar';
 
 import type { Path } from '../types/Metadatas';
 
 let lastEventTimestamp: number | null = null;
 
-const DEBOUNCE_THRESHOLD = 10;
+const DEBOUNCE_THRESHOLD_IN_MS = 10;
 
 function handleEvent(callback: () => void) {
   const currentTimestamp = Date.now();
 
-  if (lastEventTimestamp !== null && currentTimestamp - lastEventTimestamp < DEBOUNCE_THRESHOLD) return;
+  if (lastEventTimestamp !== null && currentTimestamp - lastEventTimestamp < DEBOUNCE_THRESHOLD_IN_MS) return;
 
   lastEventTimestamp = currentTimestamp;
   callback();
 }
 
 function watcher(folderPath: Path, callback: () => void): void {
-  const watcher = chokidar.watch(folderPath, { ignoreInitial: true, persistent: true });
+  const watcher = watch(folderPath, { ignoreInitial: true, persistent: true });
 
   watcher.on('all', () => {
     handleEvent(callback);
