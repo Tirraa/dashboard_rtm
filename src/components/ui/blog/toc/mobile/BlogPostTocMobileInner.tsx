8d732e4fd60eb98f18afd8dd566887a7bfa18824
GustaveWPM <jgeorge@perso.be>
2024-03-16 03:18:04 +0100

    chore: some styling on the mobile toc

--- src/components/ui/blog/toc/mobile/BlogPostTocMobileInner.tsx
+++ src/components/ui/blog/toc/mobile/BlogPostTocMobileInner.tsx
@@ -5,6 +5,7 @@ import { AccordionContent, AccordionTrigger, AccordionItem, Accordion } from '@/
 import { useScopedI18n } from '@/i18n/client';
 import { i18ns } from '##/config/i18n';
 import { capitalize } from '@/lib/str';
+import { cn } from '@/lib/tailwind';
 import Link from 'next/link';
 
 import type { BlogPostTocMobileInnerProps } from '../types';
@@ -12,7 +13,20 @@ import type { BlogPostTocMobileInnerProps } from '../types';
 const accordionContentGenerator = (headings: DocumentHeading[]): ReactElement[] =>
   headings.map((heading) => (
     <AccordionContent key={heading.slug}>
-      <Link className="text-lg font-bold transition-colors duration-200 ease-in-out hover:text-primary" href={'#' + heading.slug} replace>
+      <Link
+        className={cn('text-lg font-bold transition-colors duration-200 ease-in-out hover:text-primary', {
+          // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+          'font-medium': 3 <= heading.depth && heading.depth <= 6,
+          // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+          'ml-6': heading.depth === 5 || heading.depth === 6,
+          // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+          'ml-4': heading.depth === 4,
+          // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+          'ml-2': heading.depth === 3
+        })}
+        href={'#' + heading.slug}
+        replace
+      >
         {heading.content}
       </Link>
     </AccordionContent>
