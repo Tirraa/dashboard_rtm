ade2b64e19ba7696a5aa545306fdd98caf0f569c
GustaveWPM <jgeorge@perso.be>
2024-03-17 21:59:36 +0100

    feat: adding new logo, video

--- src/components/ui/cta/InviteTheBotButton.tsx
+++ src/components/ui/cta/InviteTheBotButton.tsx
@@ -3,6 +3,7 @@
 
 'use client';
 
+import type { WithClassname } from '@rtm/shared-types/Next';
 import type { FunctionComponent } from 'react';
 
 import { CogIcon } from '@heroicons/react/20/solid';
@@ -11,13 +12,14 @@ import { Button } from '@/components/ui/Button';
 import DISCORD_CONFIG from '@/config/discord';
 import { i18ns } from '##/config/i18n';
 
-export interface InviteTheBotButtonProps {}
+export interface InviteTheBotButtonProps extends Partial<WithClassname> {}
 
-const InviteTheBotButton: FunctionComponent<InviteTheBotButtonProps> = () => {
+const InviteTheBotButton: FunctionComponent<InviteTheBotButtonProps> = ({ className: classNameValue }) => {
   const globalT = getClientSideI18n();
+  const className = classNameValue ?? '';
 
   return (
-    <Button href={DISCORD_CONFIG.BOT_INVITE_LINK}>
+    <Button href={DISCORD_CONFIG.BOT_INVITE_LINK} className={className}>
       <span className="flex items-center gap-1">
         <span>
           <CogIcon className="h-7 w-7" />
