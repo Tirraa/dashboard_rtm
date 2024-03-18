96f7788b049db7821561ac13d401031c0334f9ad
GustaveWPM <jgeorge@perso.be>
2024-03-18 23:15:22 +0100

    fix: trading off visual glitch to preserve ssg

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,18 +9,14 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import I18nTaxonomy from '##/config/taxonomies/i18n';
 import ELEMENTS_ID from '@/config/elementsId';
 import Providers from '@/contexts/Providers';
-import { getServerSession } from 'next-auth';
 
-export default async function RootLayout({ children, params }: LayoutBaseProps) {
+export default function RootLayout({ children, params }: LayoutBaseProps) {
   const language = params[I18nTaxonomy.LANGUAGE];
-  const session = await getServerSession();
 
   return (
     <HtmlElement params={params}>
       <div id={ELEMENTS_ID.BODY_CONTAINER} className={BODY_CONTAINER_CLS}>
-        <Providers locale={language} session={session}>
-          {children}
-        </Providers>
+        <Providers locale={language}>{children}</Providers>
       </div>
       <SpeedInsights />
     </HtmlElement>
--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -3,7 +3,6 @@
 
 'use client';
 
-import type { WithSession } from '@rtm/shared-types/Next';
 import type { FunctionComponent } from 'react';
 
 import { SessionProvider } from 'next-auth/react';
@@ -13,11 +12,11 @@ import type { I18nProviderProps } from './I18nProvider';
 import I18nProvider from './I18nProvider';
 import UIProvider from './UIProvider';
 
-interface ProvidersProps extends I18nProviderProps, WithSession {}
+interface ProvidersProps extends I18nProviderProps {}
 
-const Providers: FunctionComponent<ProvidersProps> = ({ children, session, locale }) => {
+const Providers: FunctionComponent<ProvidersProps> = ({ children, locale }) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       <I18nProvider locale={locale}>
         <UIProvider>{children}</UIProvider>
       </I18nProvider>
