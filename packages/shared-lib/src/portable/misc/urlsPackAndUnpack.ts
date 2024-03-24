e80235e19e5ce4447f9ffaac3b32c5e08a77be05
GustaveWPM <jgeorge@perso.be>
2024-03-24 02:57:41 +0100

    chore: minor code review

--- packages/shared-lib/src/portable/misc/urlsPackAndUnpack.ts
+++ packages/shared-lib/src/portable/misc/urlsPackAndUnpack.ts
@@ -6,15 +6,15 @@ type EncodedString = string;
 export const MIN_ID: Id = 0;
 export const MAX_ID: Id = 0xffff;
 
+const PADDING = 2;
+
 export function packIds(unpacked: Id[]): EncodedString {
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   if (unpacked.length === 0) return '';
 
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const packed = Buffer.alloc(unpacked.length * 2);
+  const packed = Buffer.alloc(unpacked.length * PADDING);
 
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  for (let i = 0; i < unpacked.length; i++) packed.writeUInt16BE(unpacked[i], i * 2);
+  for (let i = 0; i < unpacked.length; i++) packed.writeUInt16BE(unpacked[i], i * PADDING);
   return base64url.encode(packed);
 }
 
@@ -24,7 +24,6 @@ export function unpackIds(encodedString: EncodedString): Id[] {
   const packed = base64url.toBuffer(encodedString);
   const unpacked: Id[] = [];
 
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  for (let i = 0; i < packed.length; i += 2) unpacked.push(packed.readUInt16BE(i));
+  for (let i = 0; i < packed.length; i += PADDING) unpacked.push(packed.readUInt16BE(i));
   return unpacked;
 }
