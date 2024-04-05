4deffa6868a88040138fc40129cea56f5be88375
GustaveWPM <jgeorge@perso.be>
2024-04-05 18:13:33 +0200

    chore: minor changes

--- packages/shared-lib/src/portable/html/getDirection.ts
+++ packages/shared-lib/src/portable/html/getDirection.ts
@@ -4,6 +4,7 @@
 import type { Direction } from '@rtm/shared-types/HTML';
 
 // {ToDo} Craft a Direction Provider instead of this stupid getDirection function https://www.radix-ui.com/primitives/docs/utilities/direction-provider
+// See also: https://github.com/QuiiBz/next-international/issues/393
 const getDirection = () => window.getComputedStyle(document.documentElement).direction as Direction;
 export default getDirection;
 
